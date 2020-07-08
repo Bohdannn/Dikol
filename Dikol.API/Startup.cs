f192d193df8bdc1cf35d9a5cab03f82627500a1a
Bohdan <bohdanreshetar@gmail.com>
2020-07-08 18:21:20 +0200

    Add CORS Support to the API

--- Dikol.API/Startup.cs
+++ Dikol.API/Startup.cs
@@ -61,6 +61,14 @@ namespace Dikol.API
             });
 
             services.AddSwaggerDocumentation();
+
+            services.AddCors(option =>
+            {
+                option.AddPolicy("CorsPolicy", policy =>
+                {
+                    policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200");
+                });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -78,6 +86,8 @@ namespace Dikol.API
             app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            app.UseCors("CorsPolicy");
 
             app.UseAuthorization();
 
